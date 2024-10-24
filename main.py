f363fa32e77b17957c4f11031100f8b72d7f89c5
Nkabyo4 <nkabyo13@gmail.com>
2024-10-24 13:26:12 -0500

    updated original main.py file

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-def main():
+def open_file():
 
     with open("books/frankenstein.txt") as f:
 
         file_contents = f.read()
 
     print(file_contents)
+
+def main():
+
+    open_file()
 
 main()
