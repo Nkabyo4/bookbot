eabb69a9097dca1fa1080def03b89eeb6a8e46b6
Nkabyo4 <nkabyo13@gmail.com>
2024-10-25 11:46:00 -0500

    Updated main.py file - currently working on dict_to_list

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import string
+
 def open_file():
 
     with open("books/frankenstein.txt") as f:
@@ -15,12 +17,39 @@ def count_words(file_contents):
     for x in split_words:
         count += 1
 
-    print (count)
+    print (f"{count} words found in the document")
+
+def count_characters(file_contents):
+
+    char_dict = {}
+    abc_chars = string.ascii_lowercase
+    string_words = str(file_contents)
+    string_lower = string_words.lower()
+
+    for x in abc_chars:
+
+        char_dict[x] = 0
+
+    for x in string_lower:
+
+        if x in char_dict:
+
+            char_dict[x] += 1
+    print (char_dict)
+    return char_dict
+
+def dict_to_list(char_dict):
+
+    my_dict = char_dict
+    print (my_dict)
+
+
 
 
 def main():
 
     f = open_file()
-    count_words(f)
+    count_characters(f)
+    dict_to_list(count_characters(f))
 
 main()
